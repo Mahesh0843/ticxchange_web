6e8fb78b986bfa5d5659ddc096f129e6bc130703
Mahesh0843 <singulurimahesh123@gmail.com>
2025-02-19 22:46:47 +0530

    updated

--- tailwind.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  // ... other config
-  plugins: [require("daisyui")],
-  daisyui: {
-    themes: ["light", "dark", "night", "synthwave", "dracula", "business", "luxury", "coffee"],
-  },
-} 
